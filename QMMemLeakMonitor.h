12380e97b3f0e813a4219edff58a32a83fbc23be
Jonlv <jon.lv@icloud.com>
2016-01-14 20:52:39 +0800

    Create QMMemLeakMonitor.h

---
+++ QMMemLeakMonitor.h
@@ -0,0 +1,19 @@
+//
+//  QMMemLeakMonitor.h
+//  QQMusic
+//
+//  
+//
+//  监控内存对象dealloc情况
+
+#import <Foundation/Foundation.h>
+
+@interface QMMemLeakMonitor : NSObject
+
++ (instancetype)sharedInstance;
+
+- (void)addMonitorObject:(id)object;
+- (void)removeMonitorObject:(id)object;
+- (void)addUIViewsToCheck:(UIView *)view fromVC:(UIViewController *)vc;
+- (BOOL)IfViewInWhiteList:(UIView *)view;
+@end
